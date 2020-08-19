b00f9f97013d456f21f697301c4098819d1137ff
Dravening <282864784@qq.com>
2020-08-19 10:11:10 +0800

    git init

---
+++ client/client.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"goRPC/rpcFunc"
+)
+
+func main() {
+	rpcFunc.StartClient()
+}
---
+++ server/server.go
@@ -0,0 +1,7 @@
+package main
+
+import "goRPC/rpcFunc"
+
+func main() {
+	rpcFunc.StartServer()
+}
